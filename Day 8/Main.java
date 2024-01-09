abab020713ed07a65f3af4204df294b5a34a5d29
Sumeet Jain <68370214+sumeetjain2002@users.noreply.github.com>
2024-01-09 06:19:08 +0530

    Day 8

---
+++ Day 8/Main.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Main{
+public static void main(String Args[])
+{
+
+Scanner obj=new Scanner(System.in);
+int a=obj.nextInt();
+int arr[]=new int[a];
+for(int i=0;i<arr.length;i++)
+{
+
+arr[i]=obj.nextInt();
+
+}
+
+System.out.println("Enter target");
+int x=obj.nextInt();
+// int res=fo(arr,x,0);
+int res=lo(arr,x,0);
+System.out.println("target is at: "+res);
+
+}
+
+public static int fo(int arr[], int t, int id)      // first ocurance code with better approach
+{
+    int res;
+ if(id==arr.length)
+ {
+    return -1;
+ }
+ if(arr[id]==t)
+ {
+    return id;                                 
+ }
+ else{
+    res =fo(arr,t,id+1);
+ }
+ return res;
+}
+
+public static int lo(int arr[], int t, int id)      // last occurance 
+// pahele sabse last me pauche and initalize ans with -1 ki mila nahi then aate aate agar abhi tak nahi mila toh chcek karenge and assign kara denge varna -1 ko hi chalne denge 😊
+{
+    if(id==arr.length)
+    {
+        return -1;
+    }
+    int ans=lo(arr,t,id+1);
+    if(ans<0)
+    {
+        if(arr[id]==t){
+            ans=id;
+        }
+        else {
+            return ans;
+        }
+    }
+    return ans;
+}
+}
