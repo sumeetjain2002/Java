bd526277b65eb14ae63cd0b2b91ad4e08518555b
“sumeetjain2002” <sumeetjain.com01@gmail.com>
2024-04-13 16:14:22 +0530

    updates

---
+++ CB day1/Arrays/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main{
+public static void main(String Args[])
+{
+
+Scanner obj=new Scanner(System.in);
+int arr[]=null;
+System.out.println(arr);
+
+arr=new int [5];
+System.out.println(arr);
+
+}
+}
